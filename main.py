9505a0d0f117f8d52fceaaaebaa5aa871a11360a
UsEr2235-bilder <anonym.mirniy.98@gmail.com>
2024-12-12 22:25:43 +0300

    main.py

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ if __name__ == "__main__":
     p2 = Product('Spaghetti', 3.4, 'Groceries')
     p3 = Product('Potato', 5.5, 'Vegetables')
 
-    print(p2)  # __str__
+    print(p2)  
 
     s1.add(p1, p2, p3)
 
